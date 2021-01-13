58e4103cc98cba240a12cb86b54bb5863b07a1a9
ArlingtonDawson <arlington.dawson@gmail.com>
2021-01-13 10:08:22 -0500

    Attached the camera to the player. Will need to look into if there (#1)
    a better way.

--- Assets/Scripts/Movement/PlayerRotationSystem.cs
+++ Assets/Scripts/Movement/PlayerRotationSystem.cs
@@ -7,20 +7,7 @@ public class PlayerRotationSystem : ComponentSystem
 {
     protected override void OnUpdate()
     {
-        var deltaTime = Time.DeltaTime;
-        var cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        var layerMask = LayerMask.GetMask("Floor");
 
-        if (Physics.Raycast(cameraRay, out var hit, 100, layerMask))
-        {
-            //Entities.ForEach((Transform transform, RotationComponent rotationComponet) =>
-            //{
-            //    var foward = hit.point - transform.position;
-            //    var rotation = Quaternion.LookRotation(foward);
-
-            //    rotationComponet.Value = new Quaternion(0, rotation.y, 0, rotation.w).normalized;
-            //});
-        }
     }
 }
 
